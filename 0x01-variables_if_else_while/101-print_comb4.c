fb6a1c6e68213331189df8a697c1b5a1bd398685
Alfred <alfredkinuthia87@gmail.com>
2023-07-14 19:45:15 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,21 +6,25 @@
 int main(void)
 {
 	int a = '0';
+	int b = '0';
+	int c = '0';
 
-	for (a = '0'; a <= '9'; a++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		int b = '0';
-
 		for (b = '0'; b <= '9'; b++)
 		{
-			if (!((a == b) || (b > a)))
+			for (a = '0'; a <= '9'; a++)
 			{
-				putchar(b);
-				putchar(a);
-				if (!(a == '9' && b == '8'))
+				if (!((a == b) || (b == c) || (b > a) || (c > b)))
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(c);
+					putchar(b);
+					putchar(a);
+					if (!((a == '9' && c == '7' && b == '8')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
