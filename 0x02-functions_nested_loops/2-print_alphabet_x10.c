4eb8b14097b52b9cc7afb9b87411217f27446f45
Alfred <alfredkinuthia87@gmail.com>
2023-07-19 18:33:15 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,14 @@ void print_alphabet_x10(void)
 	char a;
 	int i;
 	i = 0;
+
 	while (i < 10)
 	{
-		for (a = 'a'; a<= 'z'; a++)
+		for (a = 'a'; a <= 'z'; a++)
 		{
 			_putchar(a);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 	i++;
 }
