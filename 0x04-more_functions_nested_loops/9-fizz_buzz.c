7ce625c8367b5b4016faf8011bc8408ccf051044
Alfred <alfredkinuthia87@gmail.com>
2023-07-21 14:47:21 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,12 +14,15 @@ int main(void)
 	{
 		if ((x % 3) == 0 && (x % 5) == 0)
 			printf("FizzBuzz");
-		else if ((x % 3) ==0)
+		else if ((x % 3) == 0)
 			printf("Fizz");
 		else if ((x % 5) == 0)
 			printf("Buzz");
 		else
 			printf("%d", x);
+		if (x == 100)
+			continue;
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
